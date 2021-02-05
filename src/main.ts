06daa52dcb7447ea0574b14a58b1ac7b3def2a2a
KY-Ng <raionkurane@gmail.com>
2021-02-05 14:27:54 +0900

    apply esline style

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import * as core from '@actions/core'
-import { outdatedPackages } from "./outdated";
+import {outdatedPackages} from './outdated'
 
 async function run(): Promise<void> {
   try {
-      await outdatedPackages();
+    await outdatedPackages()
   } catch (error) {
     core.setFailed(error.message)
   }
--- src/outdated.ts
+++ src/outdated.ts
@@ -1,126 +1,135 @@
-import { exec, ExecOptions } from "@actions/exec";
+import {exec, ExecOptions} from '@actions/exec'
 
 // TODO: add tests
 
-export async function outdatedPackages() {
-    let output = '';
-    let error = '';
+export async function outdatedPackages(): Promise<void> {
+  let output = ''
+  let error = ''
 
-    const options: ExecOptions = {};
-    options.silent = true;
-    options.listeners = {
-        stdout: (data: Buffer) => {
-            output += data.toString();
-        },
-        stderr: (data: Buffer) => {
-            error += data.toString();
-        }
-    };
+  const options: ExecOptions = {}
+  options.silent = true
+  options.listeners = {
+    stdout: (data: Buffer) => {
+      output += data.toString()
+    },
+    stderr: (data: Buffer) => {
+      error += data.toString()
+    }
+  }
 
-    await exec('flutter', ['pub', 'outdated'], options);
+  await exec('flutter', ['pub', 'outdated'], options)
 
-    // TODO: error handling when error is emptyString
-    console.log(error);
+  // TODO: error handling when error is emptyString
+  // eslint-disable-next-line no-console
+  console.log(error)
 
-    let dependencies = parseFlutterPubOutdatedOutput(output);
+  const dependencies = parseFlutterPubOutdatedOutput(output)
 
-    // TODO: only returns dependencies and devDependencies
-    console.log(dependencies);
+  // TODO: only returns dependencies and devDependencies
+  // eslint-disable-next-line no-console
+  console.log(dependencies)
 }
 
-function parseFlutterPubOutdatedOutput(outputFromConsole: string): Dependencies {
-    let dependencySections = splitIntoDependencySections(outputFromConsole);
+function parseFlutterPubOutdatedOutput(
+  outputFromConsole: string
+): Dependencies {
+  const dependencySections = splitIntoDependencySections(outputFromConsole)
 
-    return {
-        dependencies: dependencySections[0],
-        devDependencies: dependencySections[1],
-        transitiveDependencies: dependencySections[2],
-        transitiveDevDependencies: dependencySections[3]
-    };
+  return {
+    dependencies: dependencySections[0],
+    devDependencies: dependencySections[1],
+    transitiveDependencies: dependencySections[2],
+    transitiveDevDependencies: dependencySections[3]
+  }
 }
 
 function splitIntoDependencySections(fullText: string): DependencySection[] {
-    // title of each dependency section
-    const dependencySections = ['Dependencies', 'dev_dependencies', 'transitive dependencies', 'transitive dev_dependencies'];
+  // title of each dependency section
+  const dependencySections = [
+    'Dependencies',
+    'dev_dependencies',
+    'transitive dependencies',
+    'transitive dev_dependencies'
+  ]
 
-    // split fullText into lines
-    let lines = splitAndRemoveEmptyString(fullText, '\n');
-    
-    // find the starting index of each section
-    let sectionStartIndexes: number[] = [];
-    for (let sectionTitle of dependencySections) {
-        let newSectionIndex = lines.findIndex(text => text.includes(sectionTitle));
-        sectionStartIndexes.push(newSectionIndex);
+  // split fullText into lines
+  const lines = splitAndRemoveEmptyString(fullText, '\n')
+
+  // find the starting index of each section
+  const sectionStartIndexes: number[] = []
+  for (const sectionTitle of dependencySections) {
+    const newSectionIndex = lines.findIndex(text => text.includes(sectionTitle))
+    sectionStartIndexes.push(newSectionIndex)
+  }
+
+  // split lines into different sections
+  const sections: string[][] = []
+  for (let i = 0; i < sectionStartIndexes.length; i++) {
+    let nextSection: string[]
+
+    // startIndex of current section
+    const startIndex = sectionStartIndexes[i]
+
+    // if current section is the last section
+    if (i + 1 === sectionStartIndexes.length) {
+      nextSection = lines.slice(startIndex)
+    } else {
+      const endIndex = sectionStartIndexes[i + 1]
+      nextSection = lines.slice(startIndex, endIndex)
     }
 
-    // split lines into different sections
-    let sections: Array<string[]> = [];
-    for (let i = 0; i < sectionStartIndexes.length; i++) {
-        let nextSection: string[];
+    sections.push(nextSection)
+  }
 
-        // startIndex of current section
-        let startIndex = sectionStartIndexes[i];
-                
-        // if current section is the last section
-        if ((i + 1) == sectionStartIndexes.length) {
-            nextSection = lines.slice(startIndex);
-        } else {
-            let endIndex = sectionStartIndexes[i + 1];
-            nextSection = lines.slice(startIndex, endIndex);
-        }
-
-        sections.push(nextSection);
-    }
-
-    return sections.map(parseIntoDependencySection);
+  return sections.map(parseIntoDependencySection)
 }
 
 function parseIntoDependencySection(section: string[]): DependencySection {
-    let dependencies: Array<string[]> = [];
+  const dependencies: string[][] = []
 
-    let rows = section.slice(1);
-    for (const row of rows) {
-        let dependencyDetails = splitAndRemoveEmptyString(row, ' ');
-        if (dependencyDetails.length == 5) {
-            dependencies.push(dependencyDetails);
-        }
+  const rows = section.slice(1)
+  for (const row of rows) {
+    const dependencyDetails = splitAndRemoveEmptyString(row, ' ')
+    if (dependencyDetails.length === 5) {
+      dependencies.push(dependencyDetails)
     }
+  }
 
-    return dependencies.map(parseIntoPackageVersionInfo);
+  return dependencies.map(parseIntoPackageVersionInfo)
 }
 
 function parseIntoPackageVersionInfo(dependency: string[]): PackageVersionInfo {
-    return {
-        packageName: dependency[0],
-        currentVersion: dependency[1],
-        upgradableVersion: dependency[2],
-        resolvableVersion: dependency[3],
-        latestVersion: dependency[4]
-    };
+  return {
+    packageName: dependency[0],
+    currentVersion: dependency[1],
+    upgradableVersion: dependency[2],
+    resolvableVersion: dependency[3],
+    latestVersion: dependency[4]
+  }
 }
 
-function splitAndRemoveEmptyString(targetString: string, separator: string): string[] {
-    return targetString
-        .split(separator)
-        .filter(element => element != '');
+function splitAndRemoveEmptyString(
+  targetString: string,
+  separator: string
+): string[] {
+  return targetString.split(separator).filter(element => element !== '')
 }
 
 interface PackageVersionInfo {
-    packageName: string,
-    currentVersion: string,
-    upgradableVersion: string,
-    resolvableVersion: string,
-    latestVersion: string
+  packageName: string
+  currentVersion: string
+  upgradableVersion: string
+  resolvableVersion: string
+  latestVersion: string
 }
 
 interface DependencySection {
-    [index: number]: PackageVersionInfo
+  [index: number]: PackageVersionInfo
 }
 
 interface Dependencies {
-    dependencies: DependencySection,
-    devDependencies: DependencySection,
-    transitiveDependencies: DependencySection,
-    transitiveDevDependencies: DependencySection
+  dependencies: DependencySection
+  devDependencies: DependencySection
+  transitiveDependencies: DependencySection
+  transitiveDevDependencies: DependencySection
 }
-
