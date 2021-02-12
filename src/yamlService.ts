3124433aeaeb4290a8003070c2b209004e291658
KY-Ng <raionkurane@gmail.com>
2021-02-12 20:12:11 +0900

    add readYaml() and writeYaml() to YamlService

--- src/yamlService.ts
+++ src/yamlService.ts
@@ -1,10 +1,29 @@
 import YAML from 'yaml'
-import {Pair, YAMLMap} from 'yaml/types'
 import * as fs from 'fs'
 
 export function readYaml(pathToYamlFile: string): YAML.Document.Parsed {
-  const yamlFile = fs.readFileSync(pathToYamlFile, 'utf-8')
-  return YAML.parseDocument(yamlFile)
+  try {
+    const yamlFile = fs.readFileSync(pathToYamlFile, 'utf-8')
+    return YAML.parseDocument(yamlFile)
+  } catch (error) {
+    throw Error(`
+    an error occured during loading of yaml file.
+    error: ${error}
+    `)
+  }
 }
 
-export function writeYaml()
+export function writeYaml(
+  yamlDoc: YAML.Document.Parsed,
+  pathToYamlFile: string
+): void {
+  try {
+    const yamlInString = yamlDoc.toString()
+    fs.writeFileSync(pathToYamlFile, yamlInString)
+  } catch (error) {
+    throw Error(`
+    an error occured during writing of yaml file.
+    error: ${error}
+    `)
+  }
+}
