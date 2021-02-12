50c20ee4083ae28518837b8f3a4390eba6f6d23b
KY-Ng <raionkurane@gmail.com>
2021-02-12 22:30:35 +0900

    reimplement and move functions (read, write, update) into one file: pubspecReader

--- src/pubspecReader.ts
+++ src/pubspecReader.ts
@@ -1,22 +1,66 @@
-import * as fs from 'fs'
-import * as yaml from 'js-yaml'
-import {Pubspec} from './interfaces'
+import YAML from 'yaml'
+import {Packages} from './interfaces'
+import {Pubspec, readYaml, writeYaml} from './yamlService'
 
-export function readPubspec(): Pubspec {
-  return readYaml('./pubspec.yaml')
+const pathToPubspec = './pubspec.yaml'
+
+export function readPubspec(): YAML.Document.Parsed {
+  return readYaml(pathToPubspec)
 }
 
-function readYaml(pathToYamlFile: string): Pubspec {
-  let doc
+export function writePubspec(pubspec: Pubspec): void {
+  writeYaml(pubspec, pathToPubspec)
+}
 
-  try {
-    doc = yaml.load(fs.readFileSync(pathToYamlFile, 'utf-8')) as Pubspec
-  } catch (error) {
-    throw Error(`
-    an error occured during loading of yaml file.
-    error: ${error}
-    `)
+export function updatePackages(
+  pubspec: Pubspec,
+  outdatedPackages: Packages
+): Pubspec {
+  let updatedPubspec = pubspec
+
+  // update dependencies
+  for (const packageInfo of outdatedPackages.dependencies) {
+    updatedPubspec = updatePackageToResolvableVersion(
+      updatedPubspec,
+      packageInfo.packageName,
+      packageInfo.resolvableVersion as string
+    )
   }
 
-  return doc
+  // update dev_dependencies
+  for (const packageInfo of outdatedPackages.devDependencies) {
+    updatedPubspec = updatePackageToResolvableVersion(
+      updatedPubspec,
+      packageInfo.packageName,
+      packageInfo.resolvableVersion as string,
+      true
+    )
+  }
+
+  return updatedPubspec
+}
+
+export function updatePackageToResolvableVersion(
+  pubspec: Pubspec,
+  packageName: string,
+  resolvableVersion: string,
+  isDevDependencies = false
+): Pubspec {
+  // doesn't update when a package is an element of packageNameToSkip
+  // as these packages have different format (type) from usual packages
+  // (there's no version related info written in them)
+  const packageNameToSkip = ['flutter', 'footer', 'flutter_test']
+  if (packageNameToSkip.includes(packageName)) {
+    return pubspec
+  }
+
+  const section = isDevDependencies ? 'dev_dependencies' : 'dependencies'
+  const packageExists = pubspec.hasIn([section, packageName])
+
+  // only update package's version when it exists
+  if (packageExists) {
+    pubspec.setIn([section, packageName], `^${resolvableVersion}`)
+  }
+
+  return pubspec
 }
