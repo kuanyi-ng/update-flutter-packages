06d8f9472dffe368b4fe6948d680efc30339b1b1
KY-Ng <raionkurane@gmail.com>
2021-02-12 23:09:37 +0900

    remove interfaces not in use;

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -12,30 +12,3 @@ export interface Packages {
   transitiveDependencies?: PackageVersionInfo[]
   transitiveDevDependencies?: PackageVersionInfo[]
 }
-
-// https://dart.dev/tools/pub/pubspec
-export interface Pubspec {
-  name: string
-  version: string
-  description: string
-  homepage?: string
-  repository?: string
-  issue_tracker?: string
-  documentation?: string
-  dependencies: PubspecDependencies
-  dev_dependencies: PubspecDependencies
-  dependency_overrides?: object
-  environment: FlutterSdk
-  executables?: object
-  publish_to?: string
-  flutter: object
-  flutter_localizations?: FlutterSdk
-}
-
-interface PubspecDependencies {
-  [index: string]: string | FlutterSdk | null
-}
-
-interface FlutterSdk {
-  sdk: string
-}
