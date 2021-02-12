c2463c2de4ab6fe81a1785553ef9f440e09bc7bc
KY-Ng <raionkurane@gmail.com>
2021-02-12 22:55:06 +0900

    update test in pubspecUpdater.test

--- __tests__/pubspecUpdater.test.ts
+++ __tests__/pubspecUpdater.test.ts
@@ -1,232 +1,119 @@
+import * as YAML from 'yaml'
 import * as interfaces from '../src/interfaces'
-import * as updater from '../src/pubspecUpdater'
+import * as pubspecService from '../src/pubspecService'
 
 describe('updatePackageToResolvableVersion', () => {
-  let pubspec: interfaces.Pubspec
+  let pubspec: YAML.Document.Parsed
 
   beforeEach(() => {
-    pubspec = {
-      name: 'test',
-      version: '0.0.0',
-      description: 'pubspec for testing',
-      environment: {sdk: '>=2.7.0 <3.0.0'},
-      dependencies: {
-        flutter: {sdk: 'flutter'},
-        cupertino_icons: '^1.0.0',
+    pubspec = YAML.parseDocument(`
+      name: 'test'
+      version: '0.0.0'
+      description: 'pubspec for testing'
+      environment:
+        sdk: '>=2.7.0 <3.0.0'
+      dependencies:
+        flutter:
+          sdk: 'flutter'
+        cupertino_icons: '^1.0.0'
         provider: '4.3.2+3'
-      },
-      dev_dependencies: {
-        effective_dart: '^1.3.0',
-        flutter_test: {sdk: 'flutter'}
-      },
-      flutter: {
-        'uses-material-design': true,
-        assets: []
-      }
-    }
+      dev_dependencies:
+        effective_dart: '^1.3.0'
+        flutter_test:
+          sdk: 'flutter'
+      flutter:
+        uses-material-design: true
+        assets:
+    `)
   })
 
   describe('dependencies', () => {
     it('updates cupertino_icons to ^2.0.0', () => {
-      const result = updater.updatePackageToResolvableVersion(
+      const result = pubspecService.updatePackageToResolvableVersion(
         pubspec,
         'cupertino_icons',
         '2.0.0'
       )
 
-      const expectedResult = {
-        name: 'test',
-        version: '0.0.0',
-        description: 'pubspec for testing',
-        environment: {sdk: '>=2.7.0 <3.0.0'},
-        dependencies: {
-          flutter: {sdk: 'flutter'},
-          cupertino_icons: '^2.0.0',
+      const expectedResult = YAML.parseDocument(`
+        name: 'test'
+        version: '0.0.0'
+        description: 'pubspec for testing'
+        environment:
+          sdk: '>=2.7.0 <3.0.0'
+        dependencies:
+          flutter:
+            sdk: 'flutter'
+          cupertino_icons: '^2.0.0'
           provider: '4.3.2+3'
-        },
-        dev_dependencies: {
-          effective_dart: '^1.3.0',
-          flutter_test: {sdk: 'flutter'}
-        },
-        flutter: {
-          'uses-material-design': true,
-          assets: []
-        }
-      }
+        dev_dependencies:
+          effective_dart: '^1.3.0'
+          flutter_test:
+            sdk: 'flutter'
+        flutter:
+          uses-material-design: true
+          assets:
+      `)
 
-      expect(result).toEqual(expectedResult)
+      expect(result.toJSON()).toStrictEqual(expectedResult.toJSON())
     })
   })
 
   describe('devDependencies', () => {
     it('updates effective_dart to ^2.0.0', () => {
-      const result = updater.updatePackageToResolvableVersion(
+      const result = pubspecService.updatePackageToResolvableVersion(
         pubspec,
         'effective_dart',
         '2.0.0',
         true
       )
 
-      const expectedResult = {
-        name: 'test',
-        version: '0.0.0',
-        description: 'pubspec for testing',
-        environment: {sdk: '>=2.7.0 <3.0.0'},
-        dependencies: {
-          flutter: {sdk: 'flutter'},
-          cupertino_icons: '^1.0.0',
-          provider: '4.3.2+3'
-        },
-        dev_dependencies: {
-          effective_dart: '^2.0.0',
-          flutter_test: {sdk: 'flutter'}
-        },
-        flutter: {
-          'uses-material-design': true,
-          assets: []
-        }
-      }
+      const expectedResult = YAML.parseDocument(`
+      name: 'test'
+      version: '0.0.0'
+      description: 'pubspec for testing'
+      environment:
+        sdk: '>=2.7.0 <3.0.0'
+      dependencies:
+        flutter:
+          sdk: 'flutter'
+        cupertino_icons: '^1.0.0'
+        provider: '4.3.2+3'
+      dev_dependencies:
+        effective_dart: '^2.0.0'
+        flutter_test:
+          sdk: 'flutter'
+      flutter:
+        uses-material-design: true
+        assets:
+    `)
 
-      expect(result).toEqual(expectedResult)
+      expect(result.toJSON()).toStrictEqual(expectedResult.toJSON())
     })
   })
 })
 
-describe('udpatePubspecDependencies', () => {
-  const pubspec = {
-    name: 'test',
-    version: '0.0.0',
-    description: 'pubspec for testing',
-    environment: {sdk: '>=2.7.0 <3.0.0'},
-    dependencies: {
-      flutter: {sdk: 'flutter'},
-      cupertino_icons: '^1.0.0',
-      provider: '4.3.2+3'
-    },
-    dev_dependencies: {
-      effective_dart: '^1.3.0',
-      flutter_test: {sdk: 'flutter'}
-    },
-    flutter: {
-      'uses-material-design': true,
-      assets: []
-    }
-  }
-
-  const dependencies: interfaces.PackageVersionInfo[] = [
-    {
-      packageName: 'cupertino_icons',
-      currentVersion: '1.0.0',
-      resolvableVersion: '2.0.0'
-    },
-    {
-      packageName: 'provider',
-      currentVersion: '4.3.2+3',
-      resolvableVersion: '5.0.0'
-    }
-  ]
-
-  it('updates upgradable dependencies', () => {
-    const result = updater.updatePubspecDependencies(pubspec, dependencies)
-    const expectedResult = {
-      name: 'test',
-      version: '0.0.0',
-      description: 'pubspec for testing',
-      environment: {sdk: '>=2.7.0 <3.0.0'},
-      dependencies: {
-        flutter: {sdk: 'flutter'},
-        cupertino_icons: '^2.0.0',
-        provider: '^5.0.0'
-      },
-      dev_dependencies: {
-        effective_dart: '^1.3.0',
-        flutter_test: {sdk: 'flutter'}
-      },
-      flutter: {
-        'uses-material-design': true,
-        assets: []
-      }
-    }
-
-    expect(result).toStrictEqual(expectedResult)
-  })
-})
-
-describe('udpatePubspecDevDependencies', () => {
-  const pubspec = {
-    name: 'test',
-    version: '0.0.0',
-    description: 'pubspec for testing',
-    environment: {sdk: '>=2.7.0 <3.0.0'},
-    dependencies: {
-      flutter: {sdk: 'flutter'},
-      cupertino_icons: '^1.0.0',
-      provider: '4.3.2+3'
-    },
-    dev_dependencies: {
-      effective_dart: '^1.3.0',
-      flutter_test: {sdk: 'flutter'}
-    },
-    flutter: {
-      'uses-material-design': true,
-      assets: []
-    }
-  }
-
-  const dependencies: interfaces.PackageVersionInfo[] = [
-    {
-      packageName: 'effective_dart',
-      currentVersion: '1.3.0',
-      resolvableVersion: '2.0.0'
-    }
-  ]
-
-  it('updates upgradable dependencies', () => {
-    const result = updater.updatePubspecDevDependencies(pubspec, dependencies)
-    const expectedResult = {
-      name: 'test',
-      version: '0.0.0',
-      description: 'pubspec for testing',
-      environment: {sdk: '>=2.7.0 <3.0.0'},
-      dependencies: {
-        flutter: {sdk: 'flutter'},
-        cupertino_icons: '^1.0.0',
-        provider: '4.3.2+3'
-      },
-      dev_dependencies: {
-        effective_dart: '^2.0.0',
-        flutter_test: {sdk: 'flutter'}
-      },
-      flutter: {
-        'uses-material-design': true,
-        assets: []
-      }
-    }
-
-    expect(result).toStrictEqual(expectedResult)
-  })
-})
-
 describe('updatePubspecToResolvableVersion', () => {
-  const pubspec = {
-    name: 'test',
-    version: '0.0.0',
-    description: 'pubspec for testing',
-    environment: {sdk: '>=2.7.0 <3.0.0'},
-    dependencies: {
-      flutter: {sdk: 'flutter'},
-      cupertino_icons: '^1.0.0',
+  const pubspec = YAML.parseDocument(`
+    name: 'test'
+    version: '0.0.0'
+    description: 'pubspec for testing'
+    environment:
+      sdk: '>=2.7.0 <3.0.0'
+    dependencies:
+      flutter:
+        sdk: 'flutter'
+      cupertino_icons: '^1.0.0'
       provider: '4.3.2+3'
-    },
-    dev_dependencies: {
-      effective_dart: '^1.3.0',
-      flutter_test: {sdk: 'flutter'}
-    },
-    flutter: {
-      'uses-material-design': true,
-      assets: []
-    }
-  }
+    dev_dependencies:
+      effective_dart: '^1.3.0'
+      flutter_test:
+        sdk: 'flutter'
+    flutter:
+      uses-material-design: true
+      assets:
+  `)
+
   const dependencies: interfaces.PackageVersionInfo[] = [
     {
       packageName: 'cupertino_icons',
@@ -252,30 +139,30 @@ describe('updatePubspecToResolvableVersion', () => {
   }
 
   it('updates both dependencies and devDependencies', () => {
-    const result = updater.updatePubspecToResolvableVersion(
+    const result = pubspecService.updatePackages(
       pubspec,
       outdatedPackages
     )
-    const expectedResult = {
-      name: 'test',
-      version: '0.0.0',
-      description: 'pubspec for testing',
-      environment: {sdk: '>=2.7.0 <3.0.0'},
-      dependencies: {
-        flutter: {sdk: 'flutter'},
-        cupertino_icons: '^2.0.0',
-        provider: '^5.0.0'
-      },
-      dev_dependencies: {
-        effective_dart: '^2.0.0',
-        flutter_test: {sdk: 'flutter'}
-      },
-      flutter: {
-        'uses-material-design': true,
-        assets: []
-      }
-    }
+    const expectedResult = YAML.parseDocument(`
+    name: 'test'
+    version: '0.0.0'
+    description: 'pubspec for testing'
+    environment:
+      sdk: '>=2.7.0 <3.0.0'
+    dependencies:
+      flutter:
+        sdk: 'flutter'
+      cupertino_icons: '^2.0.0'
+      provider: '^5.0.0'
+    dev_dependencies:
+      effective_dart: '^2.0.0'
+      flutter_test:
+        sdk: 'flutter'
+    flutter:
+      uses-material-design: true
+      assets:
+  `)
 
-    expect(result).toStrictEqual(expectedResult)
+    expect(result.toJSON()).toStrictEqual(expectedResult.toJSON())
   })
 })
